4bbf1a161ec9a311e5c0ec764881f465aa6eed3e
Wan Hamzah Iyad <92302244+iyadadalan@users.noreply.github.com>
2024-06-23 08:59:38 +0800

    Create security_utils.php

---
+++ Enhanced/security_utils.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+function sanitize_input($data) {
+    return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
+}
+
+function generate_csrf_token() {
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function verify_csrf_token($token) {
+    if (isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token)) {
+        unset($_SESSION['csrf_token']); // Invalidate the used token
+        return true;
+    }
+    return false;
+}
+?>
