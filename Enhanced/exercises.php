cd362d45863d044cc378d68b7bad2a3fd23d1c82
Wan Hamzah Iyad <92302244+iyadadalan@users.noreply.github.com>
2024-06-23 08:45:31 +0800

    Update exercises.php
    update jquery

--- Enhanced/exercises.php
+++ Enhanced/exercises.php
@@ -272,8 +272,8 @@
       </div>
     </footer>
 
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="exercises.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 </body>
 </html>
