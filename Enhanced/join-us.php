e8051fef6e22adba81c34ccf46c2e1208e665d32
Wan Hamzah Iyad <92302244+iyadadalan@users.noreply.github.com>
2024-06-23 08:40:47 +0800

    Update join-us.php

--- Enhanced/join-us.php
+++ Enhanced/join-us.php
@@ -171,7 +171,7 @@
     </div>
   </footer>
 
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="join-us.js"></script>
 </body>
 </html>
