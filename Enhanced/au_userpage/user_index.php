3dab794e5ab472b4dac03f450311fa3ba8b1ee7b
Wan Hamzah Iyad <92302244+iyadadalan@users.noreply.github.com>
2024-06-24 20:30:58 +0800

    Update user_index.php

--- Enhanced/au_userpage/user_index.php
+++ Enhanced/au_userpage/user_index.php
@@ -25,6 +25,16 @@ if ($result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="
+        default-src 'self';
+        script-src 'self';
+        style-src 'self' https://fonts.googleapis.com;
+        img-src 'self' https://images.pexels.com;
+        font-src 'self' https://fonts.gstatic.com;
+        connect-src 'self';
+        frame-src 'none';
+        object-src 'none';
+        base-uri 'self';">
     <title>SweatFactory - Home</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
